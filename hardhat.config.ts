1bd7a0838fb0cfb0bf5fa815fabc1688a8abbc55
Michael Garber <michael.garber@swirldslabs.com>
2022-12-12 17:01:16 -0500

    add hedera network config
    Signed-off-by: Michael Garber <michael.garber@swirldslabs.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,36 @@
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+dotenv.config();
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  defaultNetwork: "hedera",
+  networks: {
+    hardhat: {},
+    hedera: {
+      url: process.env.JSON_RPC_URL || "",
+      chainId: 296,
+      from: "",
+      gas: "auto",
+      gasPrice: "auto",
+      accounts: [process.env.PRIVATE_KEY || ""]
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 24000,
+      },
+    },
+  },
 };
 
 export default config;

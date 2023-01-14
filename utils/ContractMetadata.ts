a24d6ba4cde67a7569846c988407481cb089e6b5
Michael Garber <michael.garber@swirldslabs.com>
2023-01-14 07:54:34 -0500

    add utils
    Signed-off-by: Michael Garber <michael.garber@swirldslabs.com>

---
+++ utils/ContractMetadata.ts
@@ -0,0 +1,11 @@
+import { ContractService } from "../service/ContractService";
+
+export default class ClientMetadata {
+  static SUPPORTED_CONTRACTS_FOR_DEPLOYMENT = [
+    "Bank",
+  ];
+
+  static SUPPORTED_PROXY_OPTIONS = ["create", "update"];
+
+
+}

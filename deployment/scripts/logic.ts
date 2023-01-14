b7f8385cd4d9a3be65ea021cd8177c0fbe9d18c8
Michael Garber <michael.garber@swirldslabs.com>
2023-01-14 07:51:09 -0500

    logic contract script
    Signed-off-by: Michael Garber <michael.garber@swirldslabs.com>

---
+++ deployment/scripts/logic.ts
@@ -0,0 +1,46 @@
+import { Deployment } from "../../utils/deployContractOnTestnet";
+import ContractMetadata from "../../utils/ContractMetadata";
+
+import { ContractFunctionParameters } from "@hashgraph/sdk";
+import dotenv from "dotenv";
+dotenv.config();
+const {
+    CONTRACT_NAME,
+    CONTRACT_PATH
+  } = process.env;
+
+const deployment = new Deployment();
+const contractMetadata = new ContractMetadata();
+
+export async function main(_contractName: string?= null, _filePath: string) {
+    const contractName = (
+        _contractName ?? CONTRACT_NAME!
+    ).toLowerCase();
+
+    const filePath = (
+        _filePath ?? CONTRACT_PATH!
+    ).toLowerCase();
+
+    console.log(
+        `Deploying (${contractName}) contract, where file path is (${filePath})`
+    );
+    const deployedContract = await deployment.deployContractAsClient(
+        filePath,
+        new ContractFunctionParameters()
+    );
+    console.log(
+        `${contractName} deployed successfully => ${JSON.stringify(
+            deployedContract
+        )}`
+    );
+    return `Successfully deployed`;
+}
+
+if (require.main === module) {
+    main()
+        .then(() => process.exit(0))
+        .catch((error) => {
+            console.error(error);
+            process.exit(1);
+        });
+}

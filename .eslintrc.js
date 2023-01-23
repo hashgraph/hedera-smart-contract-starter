9b5d5d71cf5f6de3adfb96e156af0a7548a1ec5e
Michael Garber <michael.garber@swirldslabs.com>
2023-01-23 11:08:05 -0500

    add node to eslint config
    Signed-off-by: Michael Garber <michael.garber@swirldslabs.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  env: {
+    node: true
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'], // Your TypeScript files extension

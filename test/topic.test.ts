f6f5d21aef2b62916142b62af93bb1b8eed751cc
Michael Garber <michael.garber@swirldslabs.com>
2023-07-14 22:41:17 -0400

    update values in test file
    Signed-off-by: Michael Garber <michael.garber@swirldslabs.com>

--- test/topic.test.ts
+++ test/topic.test.ts
@@ -29,22 +29,22 @@ describe("Topic", function () {
 
   beforeEach(async function () {
     const TopicFactory = await ethers.getContractFactory("Topic");
-    Topic = await upgrades.deployProxy(TopicFactory, ["MyTopicId"], { initializer: 'initialize' });
+    Topic = await upgrades.deployProxy(TopicFactory, ["0.0.1337"], { initializer: 'initialize' });
 
     const TopicV2Factory = await ethers.getContractFactory("TopicV2");
     TopicV2 = await upgrades.upgradeProxy(Topic.address, TopicV2Factory);
   });
 
   it("should return the correct topicId", async function () {
-    expect(await (Topic as unknown as TopicContract).getTopicId()).to.equal("MyTopicId");
+    expect(await (Topic as unknown as TopicContract).getTopicId()).to.equal("0.0.1337");
   });
 
   it("should return the correct topicId for version 2", async function () {
-    expect(await (TopicV2 as unknown as TopicContract).getTopicId()).to.equal("MyTopicId");
+    expect(await (TopicV2 as unknown as TopicContract).getTopicId()).to.equal("0.0.1337");
   });
 
   it("should set and return the correct message for version 2", async function () {
-    await (TopicV2 as unknown as TopicContract).setMessage("MyMessage");
-    expect(await (TopicV2 as unknown as TopicContract).getMessage()).to.equal("MyMessage");
+    await (TopicV2 as unknown as TopicContract).setMessage("HelloFuture");
+    expect(await (TopicV2 as unknown as TopicContract).getMessage()).to.equal("HelloFuture");
   });
 });
